c074a0dbc71fa02be2e43c15a65b05492ecd37cb
Jeremy Donnell <jdonnell@gannett.com>
2016-08-31 12:42:27 -0400

    GDPAC-1950 Try fixing index-tests

--- ajwain/test/unit/index-tests.js
+++ ajwain/test/unit/index-tests.js
@@ -1,5 +1,19 @@
 describe('ajwain: index tests', function() {
+    var mockery = require('mockery');
     var expect = require('chai').expect;
+
+    before(function () {
+        mockery.enable({
+            useCleanCache: true,
+            warnOnUnregistered: false
+        });
+    });
+
+    beforeEach(function () {
+        mockery.deregisterAll();
+        mockery.resetCache();
+        mockery.registerMock('./src/job-manager.js', function () { return {};});
+    });
 
     it('should return the ajwain function on requiring index.js', function(){
         var Ajwain = require('../../index.js');
